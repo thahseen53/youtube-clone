81e07e7d6de70d37545e79ce98e3da5b37d44997
Mohammed Thahseen <61452944+thahseen53@users.noreply.github.com>
2023-05-30 19:41:58 +0530

    Update dataSevice.js

--- src/utils/dataSevice.js
+++ src/utils/dataSevice.js
@@ -1,4 +1,4 @@
-
+//add youtube api key
 
 export const YOUTUBE_VIDEOS_API =`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${YOUTUBE_API_KEY}`
 
